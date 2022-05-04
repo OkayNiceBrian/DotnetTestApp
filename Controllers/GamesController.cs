3d362f8850b008cd54d8f6d7d9dac1fdf8faa026
Brian Jackson <brianjackson1224@gmail.com>
2022-05-03 22:56:48 -0700

    Boilerplate site with a couple additions and Crosser

---
+++ Controllers/GamesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotnetTestApp.Models;
+
+namespace DotnetTestApp.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly ILogger<GamesController> _logger;
+
+        public GamesController(ILogger<GamesController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Games()
+        {
+            return View();
+        }
+        public IActionResult Chess()
+        {
+            return View();
+        }
+        public IActionResult Crosser()
+        {
+            return View();
+        }
+        public IActionResult Go()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotnetTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
